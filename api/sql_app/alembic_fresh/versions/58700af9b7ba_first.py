548eab044b6feee711d84b322fdb1b9626bfeb7d
Sampalean Sorin <sampaleansorin@outlook.com>
2024-03-26 20:46:41 +0200

    upload files

---
+++ api/sql_app/alembic_fresh/versions/58700af9b7ba_first.py
@@ -0,0 +1,24 @@
+"""first
+
+Revision ID: 58700af9b7ba
+Revises: 
+Create Date: 2022-11-09 16:46:22.034749
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '58700af9b7ba'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
